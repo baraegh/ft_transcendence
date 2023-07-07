47f020b321d39119b94d095d6328ccd34ce7a557
Mohammed Brighli <brmohamm@e2r10p4.1337.ma>
2023-07-07 18:37:24 +0100

    other profile

--- server/src/other_profile/other_profile.module.ts
+++ server/src/other_profile/other_profile.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OtherProfileController } from './other_profile.controller';
+import { OtherProfileService } from './other_profile.service';
 
-@Module({})
+@Module({
+  controllers: [OtherProfileController],
+  providers: [OtherProfileService]
+})
 export class OtherProfileModule {}
