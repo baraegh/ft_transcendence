7c74eb699cac33e3a5b7faed2a6bd6fae0e3bfb6
Mohamed Ait aadi <mait-aad@c3r10p7.1337.ma>
2023-08-16 17:38:27 +0100

    fixing error in maps.tsx

--- client/src/front-end/tsx/maps.tsx
+++ client/src/front-end/tsx/maps.tsx
@@ -1,32 +1,35 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import '../css/maps.css';
-import Edit from '../img/edit.png';
-import { FormFloating } from 'react-bootstrap';
-import axios from 'axios';
-import me from '../img/rimney.jpeg';
-import square from '../img/square.png';
-import circle from '../img/circle.png';
-import Notification from './notification';
-import { SocketContext } from '../../socket/socketContext';
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
-import map1 from '../img/map1.png';
-import map2 from '../img/map2.png';
-import map3 from '../img/map3.png';
+// import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+// import { FormFloating } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { SocketContext } from "../../socket/socketContext";
+import "../css/maps.css";
+import circle from "../img/circle.png";
+// import Edit from "../img/edit.png";
+import map1 from "../img/map1.png";
+import map2 from "../img/map2.png";
+import map3 from "../img/map3.png";
+// import me from "../img/rimney.jpeg";
+import square from "../img/square.png";
+// import Notification from "./notification";
 
 interface BlankModalProps {
   show: boolean;
   onHide: () => void;
 }
 
-
-const BlankModal: React.FC<BlankModalProps> = ({ show, onHide, socket, userId }) => {
+const BlankModal: React.FC<BlankModalProps> = ({
+  show,
+  onHide,
+  socket,
+  userId,
+}) => {
   const navigate = useNavigate();
   const [map, setMap] = useState<any[]>([]);
   const [ball, setBall] = useState("");
   useEffect(() => {
-
     // console.log('Map updated:', map);
     // console.log('Ball updated:', ball);
     // Perform any additional actions when map changes
@@ -47,20 +50,23 @@ const BlankModal: React.FC<BlankModalProps> = ({ show, onHide, socket, userId })
       image: string;
     } = {
       player2Id: Number(userId),
-      mode: { pColor: map[0].pColor  , bColor: map[0].bColor , fColor: map[0].fColor , bMode: ball },
-      name: 'von',
-      image: 'image',
+      mode: {
+        pColor: map[0].pColor,
+        bColor: map[0].bColor,
+        fColor: map[0].fColor,
+        bMode: ball,
+      },
+      name: "von",
+      image: "image",
     };
 
     if (socket && Number(userId) > 0) {
-      console.log('>>>>>>send from:' + dataToSend.mode.fColor);
-      socket.emit('sendGameRequest', dataToSend);
-      navigate('/loadingPage');
-    }
-    else if(socket && Number(userId) === 0){
-      socket.emit('quick_game',dataToSend);
-      navigate('/loadingPage');
-
+      console.log(">>>>>>send from:" + dataToSend.mode.fColor);
+      socket.emit("sendGameRequest", dataToSend);
+      navigate("/loadingPage");
+    } else if (socket && Number(userId) === 0) {
+      socket.emit("quick_game", dataToSend);
+      navigate("/loadingPage");
     }
   };
 
@@ -70,59 +76,96 @@ const BlankModal: React.FC<BlankModalProps> = ({ show, onHide, socket, userId })
         <div className="mapsHeader">Pick Your Map</div>
         <div className="maps">
           <div className="balls">
-            <div className="smallBall" onClick={() => {
-              setBall("1");
-            }}
-            style={{
-              border: ball === '1' ? '1px solid white' : '',
-              borderRadius : "60px",
-            }}          
+            <div
+              className="smallBall"
+              onClick={() => {
+                setBall("1");
+              }}
+              style={{
+                border: ball === "1" ? "1px solid white" : "",
+                borderRadius: "60px",
+              }}
             >
               <img src={circle} alt="" />
             </div>
-            <div className="bigBall" onClick={() => {
-              setBall("2");
-            }}
-            style={{
-              border: ball === '2' ? '1px solid white' : '',
-              borderRadius : "60px",
-            }}          
+            <div
+              className="bigBall"
+              onClick={() => {
+                setBall("2");
+              }}
+              style={{
+                border: ball === "2" ? "1px solid white" : "",
+                borderRadius: "60px",
+              }}
             >
               <img src={circle} alt="" />
             </div>
-            <div className="blockBall" onClick={() => {
-              setBall("3");
-            }}
-            style={{
-              border: ball === '3' ? '1px solid white' : '',
-              borderRadius : "60px",
-            }}          
+            <div
+              className="blockBall"
+              onClick={() => {
+                setBall("3");
+              }}
+              style={{
+                border: ball === "3" ? "1px solid white" : "",
+                borderRadius: "60px",
+              }}
             >
               <img src={square} alt="" />
             </div>
           </div>
           <div
-            onClick={() => setMap([{ pColor: 'WHITE', bColor: 'WHITE', fColor: 'BLACK', bMode: '' }])}
+            onClick={() =>
+              setMap([
+                {
+                  pColor: "WHITE",
+                  bColor: "WHITE",
+                  fColor: "BLACK",
+                  bMode: "",
+                },
+              ])
+            }
             className="map"
             style={{
               backgroundImage: `url(${map1})`,
-              border: map.length > 0 && map[0].fColor === 'BLACK' ? '6px solid red' : '',
+              border:
+                map.length > 0 && map[0].fColor === "BLACK"
+                  ? "6px solid red"
+                  : "",
             }}
           ></div>
           <div
-            onClick={() => setMap([{ pColor: 'BLACK', bColor: 'BLACK', fColor: 'WHITE', bMode: '' }])}
+            onClick={() =>
+              setMap([
+                {
+                  pColor: "BLACK",
+                  bColor: "BLACK",
+                  fColor: "WHITE",
+                  bMode: "",
+                },
+              ])
+            }
             className="map"
             style={{
               backgroundImage: `url(${map2})`,
-              border: map.length > 0 && map[0].fColor === 'WHITE' ? '6px solid red' : '',
+              border:
+                map.length > 0 && map[0].fColor === "WHITE"
+                  ? "6px solid red"
+                  : "",
             }}
           ></div>
           <div
-            onClick={() => setMap([{ pColor: 'BLACK', bColor: 'WHITE', fColor: 'GRAY', bMode: '' }])}
+            onClick={() =>
+              setMap([
+                { pColor: "BLACK", bColor: "WHITE", fColor: "GRAY", bMode: "" },
+              ])
+            }
             className="map"
             style={{
               backgroundImage: `url(${map3})`,
-              border: map.length > 0 && map[0].fColor === 'GRAY' ? '6px solid red' : '',
+              border:
+                map.length > 0 && map[0].fColor === "GRAY"
+                  ? "6px solid red"
+                  : "",
             }}
           ></div>
         </div>
@@ -133,14 +176,11 @@ const BlankModal: React.FC<BlankModalProps> = ({ show, onHide, socket, userId })
     </Modal>
   );
 };
-
-
 const Maps: React.FC<{ buttonText: string }> = ({ buttonText }) => {
   const [showModal, setShowModal] = useState(false);
   const { socket } = useContext<any | undefined>(SocketContext);
   let { userId } = useParams();
-  if(userId === undefined)
-    userId = "0";
+  if (userId === undefined) userId = "0";
   const handleImageClick = () => {
     setShowModal(true);
   };
@@ -151,11 +191,18 @@ const Maps: React.FC<{ buttonText: string }> = ({ buttonText }) => {
   console.log(userId);
 
   return (
-    <div>
-      <a onClick={handleImageClick}>{buttonText}</a>
+    <>
+      <span onClick={handleImageClick}>{buttonText}</span>
 
-      {showModal && <BlankModal socket={socket} userId={userId} show={showModal} onHide={handleCloseModal} />}
-    </div>
+      {showModal && (
+        <BlankModal
+          socket={socket}
+          userId={userId}
+          show={showModal}
+          onHide={handleCloseModal}
+        />
+      )}
+    </>
   );
 };
 
